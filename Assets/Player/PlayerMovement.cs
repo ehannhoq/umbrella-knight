2f21e5aa63786f4ce1c587c7ebb8660fbe24ea7f
ehann <wolfxix106@gmail.com>
2025-11-28 03:38:58 -0500

    revamped movement, fixed almost all bugs

--- Assets/Player/PlayerMovement.cs
+++ Assets/Player/PlayerMovement.cs
@@ -1,11 +1,5 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.Assertions.Must;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
@@ -16,332 +10,291 @@ public class PlayerMovement : MonoBehaviour
         Left,
         Right
     }
-    public static Action playerJumped;
-    [SerializeField] float _playerHeight;
+
+    [Header("Horizontal Movement")]
     [SerializeField] private float _movementSpeed;
-    [SerializeField] private Dictionary<string, float> _movementSpeedMultipliers;
-    [SerializeField] private float _maxStepHeight = 0.5f;
-    [SerializeField] private float _minStepHeight = 0.5f;
-    [SerializeField] private float _stickToGroundForce = 10f;
+    [SerializeField] private float _minStepHeight;
+    [SerializeField] private float _maxStepHeight;
+    [SerializeField] private float _minimumWallRideSpeed;
+    public bool runningIntoWall;
 
-    [SerializeField] float _jumpHeight;
+    [Header("Vertical Movement")]
+    [SerializeField] private float _jumpHeight;
+    [SerializeField] private float _jumpCooldown;
 
-    [Tooltip("In Seconds")]
-    [SerializeField] float _jumpCooldown;
-    Rigidbody _rb;
-    float _currentSpeed;
-    GameObject _player;
-    GameObject _cam;
-    Vector2 _moveInput;
-    Animator _animator;
-    float _linearDampening;
-    [SerializeField] int _wallRiding;
-    [SerializeField] float _wallRideGravity;
+    [Header("Gravity")]
+    [SerializeField] private float _gravity;
+    [SerializeField] private float _glidingGravity;
+    [SerializeField] private float _wallRideGravity;
+    [SerializeField] private float _stickToGroundForce;
+    [SerializeField] private float ascendingFallingThreshold;
 
-    public float gravityMultiplier;
-    public float glidingMultiplier;
-    public bool isGrounded;
-    public bool onJumpCooldown;
-    public bool canMove;
-    public bool isAscending;
-    public bool isFalling;
-    public bool isGliding;
-    public float ascendingFallingThreshold;
+    private Rigidbody _rb;
+    private Animator _anim;
+    private GameObject _camera;
+
+    private Vector2 _moveInput;
+    private RaycastHit _groundHit;
+    private int _wallRiding;
+    private const float _playerHeight = 13f;
+
+    [Header("Public Variables")]
+    public bool grounded;
+    public bool gliding;
+    public bool ascending;
+    public bool falling;
 
 
     void Start()
     {
-        _player = GameObject.FindWithTag("Player");
-        _rb = _player.GetComponent<Rigidbody>();
-        _cam = GameObject.FindWithTag("MainCamera");
-        _animator = _player.GetComponent<Animator>();
-        _movementSpeedMultipliers = new Dictionary<string, float>();
-        _linearDampening = _rb.linearDamping;
+        GameObject player = GameObject.FindWithTag("Player");
 
-        canMove = true;
+        _rb = player.GetComponent<Rigidbody>();
+        _anim = player.GetComponent<Animator>();
+        _camera = GameObject.FindWithTag("MainCamera");
 
         _rb.freezeRotation = true;
-        // Cursor.visible = false;
     }
-
 
     void FixedUpdate()
     {
-        // Debug.DrawRay(player.transform.position, -player.transform.up * 0.1f, Color.red, 1f, false);
+        CheckGrounded();
+        StickToGround();
+        HandleMovement();
+        AdjustVelocity();
+        SetAnimation();
 
-        isGrounded = Physics.CheckSphere(
-            _player.transform.position,
-            0.25f,
-            ~LayerMask.GetMask("Player")
-        );
+        _rb.linearDamping = grounded ? 10 : 1;
+        ascending = _rb.linearVelocity.y > ascendingFallingThreshold;
+        falling = _rb.linearVelocity.y < -ascendingFallingThreshold;
 
-        if (isGrounded)
-        {
-            _rb.linearDamping = _linearDampening;
-
-            RaycastHit hit;
-            if (Physics.Raycast(_player.transform.position + Vector3.up * 0.1f, Vector3.down, out hit, _minStepHeight + 0.2f))
-            {
-                float stepDifference = hit.point.y - _player.transform.position.y;
-                if (stepDifference > 0f && stepDifference <= _minStepHeight)
-                {
-                    _rb.MovePosition(new Vector3(_player.transform.position.x, hit.point.y, _player.transform.position.z));
-                }
-                else if (stepDifference < 0f)
-                {
-                    _rb.AddForce(Vector3.down * _stickToGroundForce, ForceMode.Force);
-                }
-            }
-        }
+        if (_wallRiding != 0)
+            _rb.AddForce(Physics.gravity * _wallRideGravity, ForceMode.Acceleration);
+        else if (gliding)
+            _rb.AddForce(Physics.gravity * _glidingGravity, ForceMode.Acceleration);
         else
+            _rb.AddForce(Physics.gravity * _gravity, ForceMode.Acceleration);
+    }
+
+    void CheckGrounded()
+    {
+        grounded = false;
+
+        if (Physics.SphereCast(
+            _rb.position + Vector3.up * 0.35f,
+            0.15f,
+            Vector3.down,
+            out _groundHit,
+            0.3f,
+            ~LayerMask.GetMask("Player"),
+            QueryTriggerInteraction.Ignore
+            ))
         {
-            _rb.linearDamping = 1;
-
-            Vector3 gravity = Physics.gravity;
-
-            if (isGliding) gravity *= glidingMultiplier;
-            else if (_wallRiding != 0) gravity *= _wallRideGravity;
-            else gravity *= gravityMultiplier;
-
-            _rb.AddForce(gravity, ForceMode.Acceleration);
-        }
-
-        isAscending = _rb.linearVelocity.y > ascendingFallingThreshold;
-        isFalling = _rb.linearVelocity.y < -ascendingFallingThreshold;
-
-        _animator.SetBool("Ascending", isAscending);
-        _animator.SetBool("Grounded", isGrounded);
-        _animator.SetBool("Falling", isFalling);
-        _animator.SetInteger("WallRide", _wallRiding);
-
-
-        if (canMove)
-        {
-            HandleMovement();
+            grounded = Vector3.Dot(_groundHit.normal, Vector3.up) > 0.6f;
         }
     }
 
+    void StickToGround()
+    {
+        if (!grounded) return;
+
+        float yDiff = _groundHit.point.y - _rb.transform.position.y;
+        if (yDiff > 0f && yDiff <= _minStepHeight)
+        {
+            _rb.MovePosition(new Vector3(_rb.transform.position.x, _groundHit.point.y, _rb.transform.position.z));
+        }
+        else if (yDiff < 0f)
+            _rb.AddForce(Vector3.down * _stickToGroundForce, ForceMode.Force);
+    }
 
     void HandleMovement()
     {
         if (_moveInput != Vector2.zero)
         {
-            Vector3 projectedForward = Vector3.ProjectOnPlane(_cam.transform.forward, Vector3.up).normalized;
-            Vector3 movementVector = ((new Vector3(projectedForward.x, 0f, projectedForward.z) * _moveInput.y) + (_cam.transform.right * _moveInput.x)) * _currentSpeed;
-            if (!isGrounded) movementVector *= 0.6f;
+            Vector3 cameraForward = Vector3.ProjectOnPlane(_camera.transform.forward, Vector3.up);
+            cameraForward.y = 0;
+            cameraForward = cameraForward.normalized;
 
-            Step(movementVector);
-            movementVector = AdjustForSlope(movementVector);
-            movementVector = AdjustForWall(movementVector);
+            Vector3 movement = (cameraForward * _moveInput.y + _camera.transform.right * _moveInput.x) * _movementSpeed;
+            Step(movement);
+            RotatePlayer(movement);
+            movement = AdjustForWall(movement);
 
-            movementVector -= new Vector3(_rb.linearVelocity.x, 0f, _rb.linearVelocity.z);
-
-            _rb.AddForce(movementVector, isGrounded ? ForceMode.Impulse : ForceMode.Acceleration);
-
-            SetPlayerRotationToCameraRotation(movementVector, slerp: true);
+            movement -= new Vector3(_rb.linearVelocity.x, 0, _rb.linearVelocity.z);
+            _rb.AddForce(movement, grounded ? ForceMode.VelocityChange : ForceMode.Acceleration);
         }
 
         CheckForWallRide();
-
-        _animator.SetBool("Walking", _moveInput != Vector2.zero);
     }
 
-
-    void Step(Vector3 direction)
-    {
-        if (direction.sqrMagnitude < 0.001f || !isGrounded) return;
-        Vector3 lower = _player.transform.position;
-        Vector3 upper = _player.transform.position + Vector3.up * _maxStepHeight;
-
-
-        if (Physics.Raycast(lower, direction.normalized, out RaycastHit hit, 0.75f))
-        {
-
-            if (Vector3.Dot(hit.normal, Vector3.up) > 0.1f) return;
-
-
-            if (!Physics.Raycast(upper, direction.normalized, 0.75f))
-            {
-                float stepHeight = hit.collider.bounds.max.y - _player.transform.position.y;
-                if (stepHeight < 0.1f) return;
-
-                stepHeight = Mathf.Clamp(stepHeight, 0f, _maxStepHeight);
-                _player.transform.position += new Vector3(0f, stepHeight, 0f);
-            }
-        }
-    }
-
-
-    Vector3 AdjustForSlope(Vector3 direction)
-    {
-        if (direction.sqrMagnitude < 0.001f || !isGrounded) return direction;
-
-        Physics.Raycast(_player.transform.position, Vector3.down, out RaycastHit hit, 0.75f);
-
-        return Vector3.ProjectOnPlane(direction, hit.normal);
-    }
-
-
-    Vector3 AdjustForWall(Vector3 direction)
-    {
-
-        Vector3 p1 = _player.transform.position + Vector3.up * (_playerHeight / 2f - 0.2f);
-        Vector3 p2 = _player.transform.position + Vector3.up * (_playerHeight / 2f + 0.2f);
-
-        if (Physics.CapsuleCast(
-            p1, p2,
-            0.3f,
-            direction.normalized,
-            out RaycastHit hit,
-            0.175f
-        ))
-        {
-            Vector3 horizontalNormal = hit.normal;
-            horizontalNormal.y = 0f;
-            horizontalNormal = horizontalNormal.normalized;
-
-            Vector3 newDirection = Vector3.ProjectOnPlane(direction, horizontalNormal);
-
-            if (_wallRiding != 0)
-                newDirection = newDirection.normalized * direction.magnitude;
-
-            return newDirection;
-        }
-
-        return direction;
-    }
 
     void CheckForWallRide()
     {
-        if (isGrounded || _rb.linearVelocity.sqrMagnitude <= _movementSpeed * _movementSpeed)
+        if (grounded || _moveInput == Vector2.zero || _rb.linearVelocity.sqrMagnitude < _minimumWallRideSpeed * _minimumWallRideSpeed)
         {
             _wallRiding = (int)WallRideDirection.None;
             return;
         }
 
-        Vector3 p1 = _player.transform.position + Vector3.up * (_playerHeight / 2f - 0.2f);
-
-        Vector3 right = _cam.transform.right;
+        Vector3 right = _camera.transform.right;
         Vector3 left = -right;
 
-        _wallRiding = (int)WallRideDirection.None;
 
-        if (GetWallHit(p1, right, 0.4f, out RaycastHit rightHit))
+        if (CheckWall(right, out RaycastHit rightHit))
         {
             float vertical = Vector3.Dot(rightHit.normal, Vector3.up);
             if (vertical < 0.1f)
                 _wallRiding = (int)WallRideDirection.Right;
         }
 
-        if (GetWallHit(p1, left, 0.4f, out RaycastHit leftHit))
+        else if (CheckWall(left, out RaycastHit leftHit))
         {
             float vertical = -Vector3.Dot(leftHit.normal, Vector3.up);
             if (vertical < 0.1f)
                 _wallRiding = (int)WallRideDirection.Left;
         }
+        else
+        {
+            _wallRiding = (int)WallRideDirection.None;
+        }
     }
 
-    bool GetWallHit(Vector3 p1, Vector3 direction, float maxDistance, out RaycastHit hit)
+    bool CheckWall(Vector3 direction, out RaycastHit hit)
     {
-        if (Physics.Raycast(p1, direction, out RaycastHit h, maxDistance))
-        {
-            hit = h;
+        float radius = 0.175f;
+        Vector3 top = _rb.position + Vector3.up * (_playerHeight - 0.3f);
+        Vector3 bottom = _rb.position + Vector3.up * 0.3f;
+
+        if (Physics.CapsuleCast(bottom, top, radius, direction, out hit, 1f, ~LayerMask.GetMask("Player")))
             return true;
-        }
+
         hit = new RaycastHit();
         return false;
     }
 
 
-    public void SetPlayerRotationToCameraRotation(Vector3 lookVector, bool slerp = false)
+    void Step(Vector3 direction)
     {
-        Quaternion targetRot = Quaternion.LookRotation(lookVector);
+        if (direction.sqrMagnitude < 0.001f || !grounded)
+        {
+            if (direction.sqrMagnitude < 0.001f) Debug.Log("Direction too small");
+            return;
+        }
 
-        if (slerp)
-            _player.transform.rotation = Quaternion.Slerp(_player.transform.rotation, targetRot, Time.fixedDeltaTime * 10f);
-        else
-            _player.transform.rotation = targetRot;
+        Vector3 lower = _rb.transform.position + Vector3.down * _minStepHeight;
+        Vector3 upper = _rb.transform.position + Vector3.up * _maxStepHeight;
+
+        // Color lowerColor = Color.red;
+        // Color upperColor = Color.red;
+
+        if (Physics.Raycast(lower, direction.normalized, out RaycastHit hit, 0.25f, ~LayerMask.GetMask("Player"), QueryTriggerInteraction.Collide))
+        {
+            if (Vector3.Dot(hit.normal, Vector3.up) > 0.25f) return;
+
+            // lowerColor = Color.green;
+
+            if (!Physics.Raycast(upper, direction.normalized, 0.25f, ~LayerMask.GetMask("Player")))
+            {
+                float stepHeight = hit.collider.bounds.max.y - _rb.transform.position.y;
+                if (stepHeight < _minStepHeight) return;
+                // upperColor = Color.green;
+
+                stepHeight = Mathf.Clamp(stepHeight, 0f, _maxStepHeight);
+                _rb.transform.position += new Vector3(0f, stepHeight, 0f);
+            }
+        }
+
+        // Debug.DrawLine(lower, lower + direction.normalized * 0.25f, lowerColor);
+        // Debug.DrawLine(upper, upper + direction.normalized * 0.25f, upperColor);
+
+    }
+
+    void AdjustVelocity()
+    {
+        _rb.linearVelocity = AdjustForSlope();
+        _rb.linearVelocity = AdjustForWall(_rb.linearVelocity);
+    }
+
+    Vector3 AdjustForSlope()
+    {
+        if (!grounded) return _rb.linearVelocity;
+
+        return Vector3.ProjectOnPlane(_rb.linearVelocity, _groundHit.normal);
+    }
+    Vector3 AdjustForWall(Vector3 direction)
+    {
+        runningIntoWall = false;
+        if (Physics.CapsuleCast(
+            _rb.position + Vector3.up * (_playerHeight - 0.3f),
+            _rb.position + Vector3.up * (0.3f),
+            0.175f,
+            direction.normalized,
+            out RaycastHit wallHit,
+            0.175f,
+            ~LayerMask.GetMask("Player")
+        ))
+        {
+            Vector3 normal = wallHit.normal;
+            normal.y = 0;
+            runningIntoWall = true;
+            float push = Vector3.Dot(direction, normal);
+            if (push > 0) direction -= normal * push;
+            Vector3 projected = Vector3.ProjectOnPlane(direction, normal);
+            return projected;
+        }
+
+        return direction;
+    }
+
+    void SetAnimation()
+    {
+        _anim.SetBool("Grounded", grounded);
+        _anim.SetBool("Moving", _moveInput != Vector2.zero);
+        _anim.SetBool("Ascending", ascending);
+        _anim.SetBool("Falling", falling);
+        _anim.SetInteger("WallRiding", _wallRiding);
     }
 
 
-    void Update()
-    {
-        float cummilativeSpeedMultiplier = 1f;
-        foreach (var keyValuePair in _movementSpeedMultipliers)
-            cummilativeSpeedMultiplier *= keyValuePair.Value;
 
-        _currentSpeed = _movementSpeed * cummilativeSpeedMultiplier;
+
+
+    public void RotatePlayer(Vector3 lookVector, bool instant = false)
+    {
+        Quaternion rotation = Quaternion.LookRotation(lookVector);
+        _rb.transform.rotation = instant ? rotation : Quaternion.Slerp(_rb.transform.rotation, rotation, 10f * Time.deltaTime);
     }
 
-    public void OnMove(InputValue action)
-    {
-        _moveInput = action.Get<Vector2>();
-    }
 
+    public void OnMove(InputValue input)
+    {
+        _moveInput = input.Get<Vector2>();
+    }
 
     public void OnJump()
     {
-        if (!onJumpCooldown)
+        if (grounded)
+            _rb.AddForce(Vector3.up * _jumpHeight, ForceMode.Impulse);
+        else if (_wallRiding != 0)
         {
-            onJumpCooldown = true;
-            if (isGrounded)
-            {
-                _rb.AddForce(_rb.transform.up * _jumpHeight, ForceMode.Impulse);
-            }
-            else if (_wallRiding != 0)
-            {
-                Vector3 p1 = _player.transform.position + Vector3.up * (_playerHeight / 2f - 0.2f);
-                Vector3 dir;
+            Vector3 p1 = _rb.transform.position + Vector3.up * (_playerHeight / 2f - 0.2f);
+            Vector3 dir;
 
-                if (_wallRiding == (int)WallRideDirection.Right)
-                    dir = _cam.transform.right;
-                else
-                    dir = -_cam.transform.right;
+            if (_wallRiding == (int)WallRideDirection.Right)
+                dir = _camera.transform.right;
+            else
+                dir = -_camera.transform.right;
 
-                GetWallHit(p1, dir, 0.4f, out RaycastHit hit);
+            CheckWall(dir, out RaycastHit hit);
 
-                Vector3 axis = Quaternion.AngleAxis(90f, Vector3.up) * hit.normal;
-                Quaternion rot = Quaternion.AngleAxis(-45f, axis);
+            Vector3 axis = Quaternion.AngleAxis(90f, Vector3.up) * hit.normal;
+            Quaternion rot = Quaternion.AngleAxis(-45f, axis);
 
-                Vector3 jumpVector = (rot * hit.normal).normalized * _jumpHeight;
+            Vector3 jumpVector = (rot * hit.normal).normalized * _jumpHeight;
 
-                _rb.AddForce(jumpVector, ForceMode.Impulse);
+            _rb.linearVelocity = jumpVector;
 
-                Debug.DrawLine(_player.transform.position, _player.transform.position + jumpVector, Color.yellow);
-
-            }
-            playerJumped.Invoke();
-            StartCoroutine(ResetJump());
+            // Debug.DrawLine(_player.transform.position, _player.transform.position + jumpVector, Color.yellow);
         }
-    }
-
-    IEnumerator WallJump(Vector3 jumpVector)
-    {
-        int duration = 4;
-        while (duration > 0)
-        {
-            yield return new WaitForFixedUpdate();
-            duration--;
-            _rb.AddForce(jumpVector * 10f, ForceMode.Acceleration);
-            jumpVector.y *= 0.5f;
-        }
-    }
-
-    IEnumerator ResetJump()
-    {
-        yield return new WaitForSeconds(_jumpCooldown);
-        onJumpCooldown = false;
-    }
-
-    public void AddSpeedMultiplier(string source, float multiplier)
-    {
-        _movementSpeedMultipliers.Add(source, multiplier);
-    }
-
-
-    public void RemoveSpeedMultiplier(string source)
-    {
-        if (_movementSpeedMultipliers.ContainsKey(source))
-            _movementSpeedMultipliers.Remove(source);
     }
 }
