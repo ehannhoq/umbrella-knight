3cbb65358066ffd81f5edf8ead9e46709a88b8c9
Ehan Haque <ehann.hoq@gmail.com>
2025-12-02 04:09:57 -0500

    consolidated common player stats into seperate script

---
+++ Assets/Player/PlayerStats.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Health")]
+    public float health;
+    public float maxHealth;
+    public float regen;
+
+    [Header("Movement")]
+    public float movementSpeed;
+
+    [Header("Umbrella")]
+    public float attackDamage;
+}
